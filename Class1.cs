73107f175dad75a49639c62cccca4f1d8ff8d583
24amaris <147129858+24amaris@users.noreply.github.com>
2023-10-27 21:57:43 +0300

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,33 @@
+﻿namespace _27oct1;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        const string usernameCorect = "Amaris24";
+        const string passwordCorect = "12345";
+
+        bool autentificat = false;
+
+        while (!autentificat)
+        {
+            Console.WriteLine("Introduceti username: ");
+            string username = Console.ReadLine();
+
+            Console.WriteLine("Introduceti parola: ");
+            string password = Console.ReadLine();
+
+            if (username == usernameCorect && password == passwordCorect)
+            {
+                Console.WriteLine("Welcome , " + usernameCorect);
+                autentificat = true;
+
+            }
+            else
+            {
+                Console.WriteLine("Authentication failed. Enter again");
+            }
+        }
+    }
+}
